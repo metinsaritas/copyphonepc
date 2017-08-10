d0c0e16ed41e8faf146bce7fa6d40c139bed2f25
metinsaritas <metinsaritas2@gmail.com>
2017-08-10 16:39:55 +0300

    Android yeni tasarım haliyle

--- CopyPhonePC/app/src/main/java/com/metinsaritas/copyphone_pc/ActivityFirst.java
+++ CopyPhonePC/app/src/main/java/com/metinsaritas/copyphone_pc/ActivityFirst.java
@@ -1,8 +1,11 @@
 package com.metinsaritas.copyphone_pc;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -11,30 +14,37 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.RemoteViews;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class ActivityFirst extends AppCompatActivity {
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
+
+public class ActivityFirst extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, ClipboardManager.OnPrimaryClipChangedListener {
     private SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
     private ViewPager mViewPager;
+    private NavigationView navigationView;
+
+    private ClipboardManager clipboardManager;
+    private Socket socket;
+    String lastCopied = "";
+    boolean otherCopying = false;
+    private MyNotification myNotification;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,11 +53,9 @@ public class ActivityFirst extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
+
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
@@ -56,25 +64,161 @@ public class ActivityFirst extends AppCompatActivity {
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
 
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        myNotification = new MyNotification(ActivityFirst.this);
+        RemoteViews remoteViews = myNotification.getRemoteViews();
+
+        clipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        clipboardManager.addPrimaryClipChangedListener(this);
+
+        try {
+            socket = IO.socket("http://calisma.herokuapp.com/");
+            socket.on("otherCopied", otherCopied);
+            socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(ActivityFirst.this, "Connected", Toast.LENGTH_SHORT).show();
+                            myNotification.getRemoteViews().setImageViewResource(R.id.ivNotificationStatus, R.drawable.status_green);
+                            myNotification.showNotify();
+                        }
+                    });
+
+                }
+            });
+
+            socket.on(Socket.EVENT_ERROR, new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(ActivityFirst.this, "Error", Toast.LENGTH_SHORT).show();
+                            myNotification.getRemoteViews().setImageViewResource(R.id.ivNotificationStatus, R.drawable.status_red);
+                            myNotification.showNotify();
+                        }
+                    });
+                }
+            });
+
+            socket.on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(ActivityFirst.this, "Disconnected", Toast.LENGTH_SHORT).show();
+                            myNotification.getRemoteViews().setImageViewResource(R.id.ivNotificationStatus, R.drawable.status_red);
+                            myNotification.showNotify();
+                        }
+                    });
+                }
+            });
+
+            socket.connect();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        remoteViews.setTextViewText(R.id.tvNotificationRoomId, "Not connected");
+
+        myNotification.showNotify();
+
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
+    Emitter.Listener otherCopied = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            if (args.length <= 0) {
+                return;
+            }
+
+            JSONObject jsonObject;
+            try {
+                jsonObject = (JSONObject) args[0];
+                String copiedText = jsonObject.getString("copiedText");
+                otherCopying = true;
+                clipboardManager.setPrimaryClip(ClipData.newPlainText("text", copiedText));
+            }
+            catch (Exception e) {
+                Log.d("Soket", e.getMessage());
+            }
+
+        }
+    }; @Override
+    public void onPrimaryClipChanged() {
+        Log.d("Soket","onPrimaryClipChanged");
+        if (otherCopying) {
+            otherCopying = false;
+            return;
+        }
+
+        String copiedText = clipboardManager.getText().toString();
+        if (copiedText.length() <= 0)
+            return;
+
+        JSONObject json = new JSONObject();
+        try {
+            json.put("copiedText", copiedText);
+            json.put("from","phone");
+            sendToSocket(json);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void sendToSocket(JSONObject json) throws JSONException {
+        if (socket == null || json == null || !socket.connected()) return;
+        String copiedText = json.getString("copiedText");
+        if (lastCopied.equals(copiedText)) return;
+        lastCopied = copiedText;
+
+        // ben kopyaladıysm emit et
+        socket.emit("dataCopied", json);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        myNotification.cancel();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity_first,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void clickSetting(MenuItem item) {
+        Toast.makeText(this, "tıklandı", Toast.LENGTH_SHORT).show();
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.openDrawer(Gravity.END);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
+    }
+
     public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
+
         private static final String ARG_SECTION_NUMBER = "section_number";
 
         public PlaceholderFragment() {
         }
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
         public static PlaceholderFragment newInstance(int sectionNumber) {
             PlaceholderFragment fragment = new PlaceholderFragment();
             Bundle args = new Bundle();
@@ -91,18 +235,14 @@ public class ActivityFirst extends AppCompatActivity {
             if (section == 1) {
                 rootView = inflater.inflate(R.layout.fragment_activity_first, container, false);
             } else {
-                rootView = inflater.inflate(R.layout.fragment_activity_first_section2, container, false);
+                rootView = inflater.inflate(R.layout.fragment_allcopied, container, false);
             }
             TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            //textview null geldifalan ife koyarak al
             return rootView;
         }
     }
 
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
         public SectionsPagerAdapter(FragmentManager fm) {
@@ -111,15 +251,12 @@ public class ActivityFirst extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
             return PlaceholderFragment.newInstance(position + 1);
         }
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
-            return 3;
+            return 2;
         }
     }
 }
